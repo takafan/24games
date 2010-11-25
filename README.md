21cdab19e6cceba23b8ba3d20fa6d45b136bc876
takafan <takafan@163.com>
2010-11-25 11:09:08 +0800

    man4d3s

---
+++ README.md
@@ -0,0 +1,18 @@
+24 game/Solve
+-------------
+computes an expression to solve the 24 game if possible.
+
+Using
+-----
+
+    gem install 24games
+
+    24games 5 9 5 9
+    
+Gets:
+    5 * 5 - 9 / 9
+
+Contributors
+------------
+
+written by Takafan.
--- lib/calc24.rb
+++ lib/calc24.rb
@@ -4,6 +4,7 @@ module Calc24
   module Exec
     
     class TwentyFourGamePlayer
+=begin
       EXPRESSIONS = [
         '((%d %s %d) %s %d) %s %d',
         '(%d %s (%d %s %d)) %s %d',
@@ -12,156 +13,156 @@ module Calc24
         '%d %s (%d %s (%d %s %d))',
       ]
       
-      MANEXPRESSIONS = [
+      OPERATORS = [:+, :-, :*, :/]
+=end
+      MAN4D3S = [
         # '((%d %s %d) %s %d) %s %d' 44
         # +++
-        '((%d + %d) + %d) + %d',
+        '%d + %d + %d + %d',
         # ++-
-        '((%d + %d) + %d) - %d',
+        '%d + %d + %d - %d',
         # ++*
-        '((%d + %d) + %d) * %d',
-        '((%d + %d) * %d) + %d',
-        '((%d * %d) + %d) + %d',
+        '(%d + %d + %d) * %d',
+        '(%d + %d) * %d + %d',
+        '%d * %d + %d + %d',
         # ++/
-        '((%d + %d) + %d) / %d',
-        '((%d + %d) / %d) + %d',
-        '((%d / %d) + %d) + %d',
+        '(%d + %d + %d) / %d',
+        '(%d + %d) / %d + %d',
+        '(%d / %d) + %d + %d',
         # +--
-        '((%d + %d) - %d) - %d',
+        '%d + %d - %d - %d',
         # +-*
-        '((%d + %d) - %d) * %d',
-        '((%d + %d) * %d) - %d',
-        '((%d - %d) * %d) + %d',
-        '((%d * %d) + %d) - %d',
+        '(%d + %d - %d) * %d',
+        '(%d + %d) * %d - %d',
+        '(%d - %d) * %d + %d',
+        '%d * %d + %d - %d',
         # +-/
-        '((%d + %d) - %d) / %d',
-        '((%d + %d) / %d) - %d',
-        '((%d - %d) / %d) + %d',
-        '((%d / %d) + %d) - %d',
+        '(%d + %d - %d) / %d',
+        '(%d + %d) / %d - %d',
+        '(%d - %d) / %d + %d',
+        '%d / %d + %d - %d',
         # +**
-        '((%d + %d) * %d) * %d',
-        '((%d * %d) + %d) * %d',
-        '((%d * %d) * %d) + %d',
+        '(%d + %d) * %d * %d',
+        '(%d * %d + %d) * %d',
+        '%d * %d * %d + %d',
         # +//
-        '((%d + %d) / %d) / %d',
-        '((%d / %d) + %d) / %d',
-        '((%d / %d) / %d) + %d',
+        '(%d + %d) / %d / %d',
+        '(%d / %d + %d) / %d',
+        '%d / %d / %d + %d',
         # ---
-        '((%d - %d) - %d) - %d',
+        '%d - %d - %d - %d',
         # --*
-        '((%d - %d) - %d) * %d',
-        '((%d - %d) * %d) - %d',
-        '((%d * %d) - %d) - %d',
+        '(%d - %d - %d) * %d',
+        '(%d - %d) * %d - %d',
+        '%d * %d - %d - %d',
         # --/
-        '((%d - %d) - %d) / %d',
-        '((%d - %d) / %d) - %d',
-        '((%d / %d) - %d) - %d',
+        '(%d - %d - %d) / %d',
+        '(%d - %d) / %d - %d',
+        '%d / %d - %d - %d',
         # -**
-        '((%d - %d) * %d) * %d',
-        '((%d * %d) - %d) * %d',
-        '((%d * %d) * %d) - %d',
+        '(%d - %d) * %d * %d',
+        '(%d * %d - %d) * %d',
+        '%d * %d * %d - %d',
         # -*/
-        '((%d - %d) * %d) / %d',
-        '((%d * %d) - %d) / %d',
-        '((%d * %d) / %d) - %d',
-        '((%d / %d) - %d) * %d',
+        '(%d - %d) * %d / %d',
+        '(%d * %d - %d) / %d',
+        '%d * %d / %d - %d',
+        '(%d / %d - %d) * %d',
         # -//
-        '((%d - %d) / %d) / %d',
-        '((%d / %d) - %d) / %d',
-        '((%d / %d) / %d) - %d',
+        '(%d - %d) / %d / %d',
+        '(%d / %d - %d) / %d',
+        '%d / %d / %d - %d',
         # ***
-        '((%d * %d) * %d) * %d',
+        '%d * %d * %d * %d',
         # **/
-        '((%d * %d) * %d) / %d',
+        '%d * %d * %d / %d',
         # *//
-        '((%d * %d) / %d) / %d',
+        '%d * %d / %d / %d',
         # ///
-        '((%d / %d) / %d) / %d',
+        '%d / %d / %d / %d',
         
         # '(%d %s (%d %s %d)) %s %d' 8
-        '(%d - (%d * %d)) * %d',
-        '(%d - (%d / %d)) * %d',
-        '(%d / (%d + %d)) * %d',
-        '(%d / (%d - %d)) * %d',
-        '(%d - (%d * %d)) / %d',
-        '(%d - (%d / %d)) / %d',
-        '(%d / (%d + %d)) / %d',
-        '(%d / (%d - %d)) / %d',
+        '(%d - %d * %d) * %d',
+        '(%d - %d / %d) * %d',
+        '%d / (%d + %d) * %d',
+        '%d / (%d - %d) * %d',
+        '(%d - %d * %d) / %d',
+        '(%d - %d / %d) / %d',
+        '%d / (%d + %d) / %d',
+        '%d / (%d - %d) / %d',
 
         # '(%d %s %d) %s (%d %s %d)' 15
-        '(%d * %d) + (%d * %d)',
-        '(%d * %d) + (%d / %d)',
-        '(%d / %d) + (%d / %d)',
-        '(%d * %d) - (%d * %d)',
-        '(%d * %d) - (%d / %d)',
-        '(%d / %d) - (%d * %d)',
-        '(%d / %d) - (%d / %d)',
+        '%d * %d + %d * %d',
+        '%d * %d + %d / %d',
+        '%d / %d + %d / %d',
+        '%d * %d - %d * %d',
+        '%d * %d - %d / %d',
+        '%d / %d - %d * %d',
+        '%d / %d - %d / %d',
         '(%d + %d) / (%d + %d)',
         '(%d + %d) / (%d - %d)',
         '(%d - %d) / (%d + %d)',
         '(%d - %d) / (%d - %d)',
-        '(%d * %d) / (%d + %d)',
-        '(%d * %d) / (%d - %d)',
-        '(%d / %d) / (%d + %d)',
-        '(%d / %d) / (%d - %d)',
+        '%d * %d / (%d + %d)',
+        '%d * %d / (%d - %d)',
+        '%d / %d / (%d + %d)',
+        '%d / %d / (%d - %d)',
         
         # '%d %s ((%d %s %d) %s %d)' 14
         '%d - ((%d + %d) * %d)',
         '%d - ((%d + %d) / %d)',
         '%d - ((%d - %d) * %d)',
         '%d - ((%d - %d) / %d)',
-        '%d - ((%d * %d) * %d)',
-        '%d - ((%d * %d) / %d)',
-        '%d - ((%d / %d) / %d)',
-        '%d / ((%d + %d) + %d)',
-        '%d / ((%d + %d) - %d)',
-        '%d / ((%d - %d) - %d)',
-        '%d / ((%d * %d) + %d)',
-        '%d / ((%d * %d) - %d)',
-        '%d / ((%d / %d) + %d)',
-        '%d / ((%d / %d) - %d)',
+        '%d - (%d * %d * %d)',
+        '%d - (%d * %d / %d)',
+        '%d - (%d / %d / %d)',
+        '%d / (%d + %d + %d)',
+        '%d / (%d + %d - %d)',
+        '%d / (%d - %d - %d)',
+        '%d / (%d * %d + %d)',
+        '%d / (%d * %d - %d)',
+        '%d / (%d / %d + %d)',
+        '%d / (%d / %d - %d)',
         
         # '%d %s (%d %s (%d %s %d))' 4
         '%d - (%d / (%d + %d))',
         '%d - (%d / (%d - %d))',
-        '%d / (%d - (%d * %d))',
-        '%d / (%d - (%d / %d))'
+        '%d / (%d - %d * %d)',
+        '%d / (%d - %d / %d)'
       ]
       
-      OPERATORS = [:+, :-, :*, :/]
-     
+      MASK = {'11' => 'J', '12' => 'Q', '13' => 'K'}
+      
       @@objective = Rational(24,1)
      
       def initialize(digits)
         @digits = digits
-        @solutions = []
-        @s = {}
+        #@solutions = []
+        @solutions = {}
         solve
       end
      
-      attr_reader :digits, :solutions, :s
+      attr_reader :digits, :solutions
      
       def solve
         digits.permutation.to_a.uniq.each do |a,b,c,d|
           #OPERATORS.each   do |op1| 
           #OPERATORS.each   do |op2| 
           #OPERATORS.each   do |op3|
-          MANEXPRESSIONS.each do |expr|
+          MAN4D3S.each do |expr|
             # evaluate using rational arithmetic
             #test = expr.gsub('%d', 'Rational(%d,1)') % [a, op1, b, op2, c, op3, d]
             test = expr.gsub('%d', 'Rational(%d,1)') % [a, b, c, d]
             value = eval(test) rescue -1  # catch division by zero
             if value == @@objective
               #@solutions << expr % [a, op1, b, op2, c, op3, d]
-              @solutions << expr % [a, b, c, d]
+              @solutions[expr] = expr % [a, b, c, d]
             end
           end#;end;end;end
         end
         
-        @solutions.each do |s|
-          s.gsub!(/11/, 'J')
-          s.gsub!(/12/, 'Q')
-          s.gsub!(/13/, 'K')
+        @solutions.values.each do |s|
+          MASK.each {|digit, poker| s.gsub!(digit, poker)}
         end
       end
     end
